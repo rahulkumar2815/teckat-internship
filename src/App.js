674539b510d3599db10f6d16699ce2340f8ed658
rahulkumar2815 <rahulkumarchohan1234567@gmail.com>
2024-06-05 18:38:05 +0530

    day4

--- src/App.js
+++ src/App.js
@@ -26,14 +26,54 @@ function App() {
   //    else{
   //     console.log("hello ")
   //   } 
-  const arr1 =[1,2,3,4,5,6];
-  for (let item of arr1){
-    console.log(item);
-  }
+  // const arr1 =[1,2,3,4,5,6];
+  // for (let item of arr1){
+  //   console.log(item);
+  // }
+// const student =
+// [
+//  {student_Name: "rahul kumar", age:18},
+// {student_Name: "sujit kumar", age:20},
+// {student_Name: "ram kumar", age:19}
+// ];
 
+// student.forEach((person)=>{
+//   console .log(person.student_Name) 
+// });
+// const student1 = {
+//   name: "sunil",
+//   age: 20,
+//   occupation:"B.Tech",
+// };
+// console .log(student1["age"]);
 
+// const student = [
+//   {name: "sunil",age:20,fee:48000},
+//   {name:"vishal",age:22,fee:4800},
+//   {name:"rahul",age:20,fee:4800},
+//   {name:"ram",age:21,fee:4800},
+// ];
+// const mappedStudent =student.map((item) =>{
+//   // console.log("item",item);
+//  return {
 
-  return (
+// ...item,
+// fee: item.fee + 100,
+// };
+// },[])
+// console.log("student",student);
+// console.log("mappedstudent",mappedStudent );
+
+// const  employee = [
+//   {name: "vishal", age: 40, salary: 10,category:" mobile"},
+//   {name: "parth", age: 90, salary: 50,category: "cloth"},
+//   {name: "shivam", age: 50, salary: 20,category: "laptop"},   
+//   ];
+//   const filteredEmployee = employee.filter((item)=>{
+//     return item.category==="mobile";
+//   });
+//   console.log("filteredemployee",filteredEmployee);
+  return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
